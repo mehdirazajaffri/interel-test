713ad65f38a89b40451f64da16b2b7f401b221d0
mehdirazajaffri <mehdirazajaffri@gmail.com>
2021-08-17 12:36:07 +0400

    Include Notification server as well

---
+++ notification-server/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require("ws");
+const redis = require("redis");
+
+const REDIS_SERVER = "redis://localhost:6379";
+const WEB_SOCKET_PORT = 3000;
+
+var redisClient = redis.createClient(REDIS_SERVER);
+redisClient.subscribe("devices");
+
+const server = new WebSocket.Server({ port: WEB_SOCKET_PORT });
+
+server.on("connection", function connection(ws) {
+  redisClient.on("message", function (channel, message) {
+    console.log(message);
+    ws.send(message);
+  });
+});
+
+console.log("WebSocket server started at ws://locahost:" + WEB_SOCKET_PORT);
+
